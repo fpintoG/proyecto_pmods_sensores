d236949a4337d211a46c9aeabb542f0d8ef1aa17
fpintoG <felipe.pinto.12@sansano.usm.cl>
2019-06-06 02:22:51 -0400

    se agrega script en matlab para monitorear lectura de sensores desde el host

---
+++ client_project.m
@@ -0,0 +1,40 @@
+%client side implementation
+
+%sensor buffers
+IRDistBuff = nan(1,100);
+USDistBuff = nan(1,100);
+fusionDataBuff = nan(1,100);
+
+%set up Plot
+plotGraph = plot(IRDistBuff,'-r' );  
+hold on                            
+plotGraph1 = plot(USDistBuff,'-b');
+hold on
+plotGraph2 = plot(fusionDataBuff,'-g' );
+title('Sensor distance data','FontSize',15);
+xlabel('time','FontSize',15);
+ylabel('disntance [cm]','FontSize',15);
+legend(legend1,legend2,legend3)
+yaxis([0 150]);
+grid(plotGrid);
+
+%server connection
+t = tcpclient('192.168.1.10', 7);
+
+while(1)
+    %ask the server for sensor data
+    write(t,1);
+    IRSensor = native2unicode(read(t,8))
+    USSensor = native2unicode(read(t,8))
+    fusionData = native2unicode(read(t,8))
+    
+    %plot realtime data
+    IRDistBuff = [IRDistBuff(2:end) IRSensor];
+    USDistBuff = [USDistBuff(2:end) USSensor];
+    fusionDataBuff = [fusionDataBuff(2:end) fusionData];
+    
+    set(plotGraph,'YData',IRDistBuff);
+    set(plotGraph1,'YData',USDistBuff);
+    set(plotGraph2,'YData',fusionDataBuff);
+    pause(1);
+end
